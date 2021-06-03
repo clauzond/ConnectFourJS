18fa76777eba62b3211d8f64484a1390c8608a87
Damien <thecsmine@gmail.com>
2021-06-03 12:55:34 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,29 +1,44 @@
 // Game parameters
-const FPS = 30;
-const HEIGHT = 700;
-const WIDTH = 640;
-const GRID_SIZE = 7; // number of rows and columns
+let FPS = 30;
+let HEIGHT = 700;
+let WIDTH = 640;
+let GRID_SIZE = 7; // number of rows and columns
 
-const SIDE_MARGIN = 20;
-const CELL = ((WIDTH - 2*SIDE_MARGIN) / GRID_SIZE); // size of cell ; 1 side margin on left and right
-const TOP_MARGIN = HEIGHT - (GRID_SIZE * CELL) - SIDE_MARGIN;
-const STROKE = CELL / 10; // stroke width (stroke=contour)
-const TILE_STROKE = CELL / 15;
-const DOT = STROKE; // dot radius
+let SIDE_MARGIN = 20;
+let CELL = ((WIDTH - 2 * SIDE_MARGIN) / GRID_SIZE); // size of cell ; 1 side margin on left and right
+let TOP_MARGIN = HEIGHT - (GRID_SIZE * CELL) - SIDE_MARGIN;
+let STROKE = CELL / 10; // stroke width (stroke=contour)
+let TILE_STROKE = CELL / 15;
+let DOT = STROKE; // dot radius
 
-const PLAYERTURN_X = WIDTH / 2;
-const PLAYERTURN_Y = TOP_MARGIN / 2;
+let PLAYERTURN_X = WIDTH / 2;
+let PLAYERTURN_Y = TOP_MARGIN / 2;
 
-const PADDING = 20; // padding in style.css - #myCanvas
+let PADDING = 20; // padding in style.css - #myCanvas
+
+function initGrid() {
+    FPS = 30;
+    HEIGHT = 700;
+    WIDTH = 640;
+    GRID_SIZE = GET_GRID_SIZE();
+    SIDE_MARGIN = 20;
+    CELL = ((WIDTH - 2 * SIDE_MARGIN) / GRID_SIZE);
+    TOP_MARGIN = HEIGHT - (GRID_SIZE * CELL) - SIDE_MARGIN;
+    STROKE = CELL / 10;
+    TILE_STROKE = CELL / 15;
+    DOT = STROKE;
+    PLAYERTURN_X = WIDTH / 2;
+    PLAYERTURN_Y = TOP_MARGIN / 2;
+}
 
 // Colors
-const COLOR_BOARD = "#497ac8";
-const COLOR_BORDER = "#1348a2";
+const COLOR_BOARD = "#8AAAE5";
+const COLOR_BORDER = "#0063B2FF";
 const COLOR_TILE = "#2C73F7";
-const COLOR_OUTER_TILE = "#1348a2";
-const COLOR_YELLOW = "#fdef08";
-const COLOR_RED = "#d5030e";
-const COLOR_TIE = "#aaaaaa";
+const COLOR_OUTER_TILE = COLOR_BORDER;
+const COLOR_YELLOW = "#FCE77D";
+const COLOR_RED = "#F7545C";
+const COLOR_TIE = "#EEEEFF";
 
 // Game Canvas
 var canv = document.getElementById("myCanvas");
@@ -32,18 +47,38 @@ canv.width = WIDTH;
 document.body.appendChild(canv);
 
 // Other elements
-// infoContainer = document.getElementById("infoContainer");
+infoContainer = document.getElementById("infoContainer");
 
 // Context
 var ctx = canv.getContext("2d");
 
 // Game variables
-var playerTurn, diskList, moveList;
+var PLAYER_TURN, STARTING_PLAYER, DISK_LIST, DISK_PER_COLUMN, MOVE_LIST;
 var NUMBER_OF_YELLOW = 0;
 var NUMBER_OF_RED = 0;
 var HIGHLIGHT_HOVER = () => { return document.getElementById("highlightHover").checked };
-var HIGHLIGHT_CAPTURED = () => { return document.getElementById("highlightCaptured").checked };
-var HIGHLIGHT_POSSIBLE = () => { return document.getElementById("highlightPossible").checked };
+var HIGHLIGHT_COLUMN = () => { return document.getElementById("highlightColumn").checked };
+var GET_ALPHA = () => { return parseFloat(document.getElementById("alpha").value) };
+var GET_GRID_SIZE = () => { return parseInt(document.getElementById("gridSize").value) };
+var SWAP_BUTTON = document.getElementById("swapTurn");
+SWAP_BUTTON.style.color = COLOR_YELLOW;
+SWAP_BUTTON.onclick = () => {
+    if (!PLAYER_TURN) {
+        return;
+    } else if (PLAYER_TURN == "yellow") {
+        PLAYER_TURN = "red";
+        SWAP_BUTTON.style.color = COLOR_RED;
+    } else {
+        PLAYER_TURN = "yellow"
+        SWAP_BUTTON.style.color = COLOR_YELLOW;
+    }
+
+    var player = (PLAYER_TURN == "yellow" ? "Yellow" : "Red");
+    var sentence = player + " is now playing !";
+
+    clearHighlight();
+    overwriteGameInfo(sentence);
+}
 
 // Event handlers
 canv.addEventListener("mousemove", highlightGrid);
@@ -52,22 +87,22 @@ canv.addEventListener("mouseleave", clearHighlight);
 
 function registerMove(row, col) {
     const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H"];
-    const moveName = alphabet[col]+""+row;
-    moveList.push(moveName);
+    const moveName = alphabet[col] + "" + row;
+    MOVE_LIST.push(moveName);
 }
 
 function highlightGrid(/** @type {MouseEvent} */ event) {
-    if (!playerTurn) {
+    if (!PLAYER_TURN) {
         return
     }
 
     var canvRect = canv.getBoundingClientRect(); // get mouse position relatively to the canvas
     // get mouse position relative to the canvas
     let x = event.clientX - canvRect.left;
-    let y = event.clientY - canvRect.top - PADDING; // padding_top
+    let y = event.clientY - canvRect.top - PADDING; // remove padding_top
 
     // highlight the possible disk and/or the captured disks
-    if (HIGHLIGHT_HOVER() || HIGHLIGHT_CAPTURED() || HIGHLIGHT_POSSIBLE()) {
+    if (HIGHLIGHT_HOVER() || HIGHLIGHT_COLUMN()) {
         // clear previous highlights
         clearHighlight();
 
@@ -76,100 +111,78 @@ function highlightGrid(/** @type {MouseEvent} */ event) {
         var RowCol = getGridRowCol(x, y);
         if (RowCol) {
             [row, col] = RowCol;
-            var disk = diskList[row][col];
-            var capturedList = isTilePossible(disk);
-            var bool = (capturedList.length != 0);
-        } else {
-            var disk = undefined;
-            var bool = false;
-            var capturedList = [];
+            if (HIGHLIGHT_HOVER()) {
+                highlightHovered(col);
+            }
+            if (HIGHLIGHT_COLUMN()) {
+                highlightColumn(col);
+            }
         }
     }
-
-    if (HIGHLIGHT_HOVER() && bool) {
-        highlightHovered(disk);
-    }
-    if (HIGHLIGHT_CAPTURED() && bool) {
-        highlightCaptured(capturedList);
-    }
-    if (HIGHLIGHT_POSSIBLE()) {
-        highlightPossible();
-    }
-
-
 }
 
 function clearHighlight() {
-    for (let row of diskList) {
+    if (!PLAYER_TURN) {
+        return;
+    }
+    for (let row of DISK_LIST) {
         for (let disk of row) {
             disk.highlight_state = false;
         }
     }
 }
 
-function highlightHovered(disk) {
-    disk.highlight_state = true;
+function highlightHovered(col) {
+    possibleRow = getPossibleRow(col);
+    if (possibleRow >= 0) {
+        DISK_LIST[possibleRow][col].highlight_state = true;
+    }
 }
 
-function highlightCaptured(capturedList) {
-    for (let disk of capturedList) {
-        disk.highlight_state = true;
+function highlightColumn(col) {
+    for (let row = 0; row <= getPossibleRow(col); row++) {
+        DISK_LIST[row][col].highlight_state = true;
     }
 }
 
 function highlightPossible() {
-    var possibleList = [];
-    for (let row of diskList) {
-        for (let disk of row) {
-            if (isTilePossible(disk).length != 0) {
-                possibleList.push(disk);
-            }
-        }
-    }
-    for (let disk of possibleList) {
-        disk.highlight_state = true;
-    }
+    return;
 }
 
 function mouseClick(/** @type {MouseEvent} */ event) {
-    if (!playerTurn) {
+    if (!PLAYER_TURN) {
         return
     }
 
     var canvRect = canv.getBoundingClientRect(); // get mouse position relatively to the canvas
     // get mouse position relative to the canvas
     let x = event.clientX - canvRect.left;
-    let y = event.clientY - canvRect.top - PADDING;
+    let y = event.clientY - canvRect.top - PADDING; // remove padding_top
 
     var RowCol = getGridRowCol(x, y);
-    console.log(RowCol);
     if (RowCol) {
         [row, col] = RowCol;
-        var disk = diskList[row][col];
-        var capturedList = isTilePossible(disk);
-        var bool = (capturedList != 0);
-        if (bool) {
-            disk.state = playerTurn;
-            playTurn(capturedList, event);
-            registerMove(row, col);
-        }
+        playTurn(row, col, event);
+        registerMove(row, col);
     }
 }
 
 function nextTurn() {
-    if (playerTurn == "yellow") {
-        playerTurn = "red";
+    if (!PLAYER_TURN) {
+        return;
+    } else if (PLAYER_TURN == "yellow") {
+        PLAYER_TURN = "red";
     } else {
-        playerTurn = "yellow";
+        PLAYER_TURN = "yellow";
     };
 }
 
 function getGridX(col) {
-    return (SIDE_MARGIN + CELL * col) // start after magin
+    return (SIDE_MARGIN + CELL * col); // start after magin
 }
 
 function getGridY(row) {
-    return (TOP_MARGIN + CELL * row) // start after margin
+    return (TOP_MARGIN + CELL * row); // start after margin
 }
 
 function getGridRowCol(x, y) {
@@ -182,10 +195,15 @@ function getGridRowCol(x, y) {
 
     // Inside the board
     var col = Math.floor((x - SIDE_MARGIN) / CELL);
-    var row = Math.floor((y - TOP_MARGIN) / CELL);
+    var row = getPossibleRow(col);
+    if (row < 0) {
+        return (null);
+    }
+    return [row, col];
+}
 
-
-    return [row, col]
+function getPossibleRow(col) {
+    return ((GRID_SIZE - 1) - DISK_PER_COLUMN[col]);
 }
 
 function drawBoard() {
@@ -205,13 +223,12 @@ function drawGrid() {
 }
 
 function drawTile(x, y) {
-    // TODO: Draw tile in a circle pattern
-    drawDisk(x + CELL / 2, y + CELL / 2, COLOR_TILE);
+    drawDisk(x + CELL / 2, y + CELL / 2, COLOR_BOARD);
     drawCircle(x + CELL / 2, y + CELL / 2, COLOR_OUTER_TILE);
 }
 
 function drawDisks() {
-    for (let row of diskList) {
+    for (let row of DISK_LIST) {
         for (let disk of row) {
             disk.draw();
             disk.highlight();
@@ -223,7 +240,7 @@ function drawDisk(x, y, color, alpha = 1) {
     ctx.fillStyle = color;
     ctx.globalAlpha = alpha;
     ctx.beginPath();
-    ctx.arc(x, y, (CELL / 2) * 0.8, 0, Math.PI * 2);
+    ctx.arc(x, y, (CELL / 2) * 0.8 + 1, 0, Math.PI * 2);
     ctx.fill();
     ctx.globalAlpha = 1;
 }
@@ -232,9 +249,9 @@ function drawCircle(x, y, color, alpha = 1) {
     // TODO: stroke width?
     ctx.fillStyle = color;
     ctx.globalAlpha = alpha;
-    ctx.lineWidth = STROKE/2;
+    ctx.lineWidth = STROKE / 2;
     ctx.beginPath();
-    ctx.arc(x, y, (CELL / 2) * 0.8 + STROKE/2 - 1, 0, Math.PI * 2);
+    ctx.arc(x, y, (CELL / 2) * 0.8 + STROKE / 2 - 1, 0, Math.PI * 2);
     ctx.stroke();
     ctx.globalAlpha = 1;
     ctx.lineWidth = STROKE;
@@ -242,19 +259,19 @@ function drawCircle(x, y, color, alpha = 1) {
 
 function drawText() {
     ctx.font = "48px Garamond";
-    ctx.fillStyle = getColor(playerTurn);
+    ctx.fillStyle = getColor(PLAYER_TURN);
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    var turn = (playerTurn == "yellow" ? "Yellow plays" : "Red plays");
+    var turn = (PLAYER_TURN == "yellow" ? "Yellow plays" : "Red plays");
     ctx.fillText(turn, PLAYERTURN_X, PLAYERTURN_Y, WIDTH);
 }
 
-function drawWinText(player, winCondition, playerTurn) {
+function drawWinText(player, winCondition) {
     let screenText, infoText, winSentence;
 
-    let x = playerTurn[0].toUpperCase() + playerTurn.substring(1);
+    let x = PLAYER_TURN[0].toUpperCase() + PLAYER_TURN.substring(1);
     infoText = "Game ended. ";
-    infoText += (winCondition == "boardFull" ? "The board is full. " : x+" cannot place any disk. ");
+    infoText += (winCondition == "boardFull" ? "The board is full. " : "");
 
     if (player == "tie") {
         screenText = "IT'S A TIE !";
@@ -268,17 +285,16 @@ function drawWinText(player, winCondition, playerTurn) {
     drawBoard();
     drawGrid();
     drawDisks();
-    playerTurn = null;
+    PLAYER_TURN = "";
     ctx.font = "48px Garamond";
     ctx.fillStyle = getColor(player);
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.fillText(screenText, PLAYERTURN_X, PLAYERTURN_Y, WIDTH);
-
 }
 
-function getColor(playerTurn) {
-    switch (playerTurn) {
+function getColor(state) {
+    switch (state) {
         case "yellow":
             return (COLOR_YELLOW);
             break;
@@ -305,121 +321,94 @@ function Disk(row, col) {
 
 
     this.draw = () => {
-        if (getColor(this.state) && playerTurn) {
+        if (getColor(this.state) && PLAYER_TURN) {
             drawDisk(this.x + CELL / 2, this.y + CELL / 2, getColor(this.state));
         }
     }
 
     this.highlight = () => {
-        var alpha = document.getElementById("alpha").value;
-        if (this.highlight_state && playerTurn) {
-            drawDisk(this.x + CELL / 2, this.y + CELL / 2, getColor(playerTurn), alpha);
+        var alpha = GET_ALPHA();
+        if (this.highlight_state && PLAYER_TURN) {
+            var possibleRow = getPossibleRow(this.col);
+            if (this.row == possibleRow && HIGHLIGHT_HOVER()) {
+                drawDisk(this.x + CELL / 2, this.y + CELL / 2, getColor(PLAYER_TURN), alpha);
+            } else if (this.row <= possibleRow) {
+                drawDisk(this.x + CELL / 2, this.y + CELL / 2, COLOR_OUTER_TILE, alpha);
+            }
         }
     }
 
 }
 
 // Game rules
-function isTilePossible(disk) {
-    if (disk.state != null) {
-        return ([])
-    }
-
-    var capturedList = [];
-
-    for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-            if (!((i == 0) && (j == 0))) {
-                // add captured disks to capturedList
-                capturedList = capturedList.concat(verifyTile_withDirection(disk, i, j))
-            }
-        }
-    }
-    return (capturedList)
-}
-
-function verifyTile_withDirection(disk, offset_row, offset_col) {
-    let list = []; // disk list to add to capturedList
-    let yourColor = (playerTurn == "yellow") ? "yellow" : "red";
-    let otherColor = (playerTurn == "yellow") ? "red" : "yellow";
-    let thisRow = disk.row + offset_row;
-    let thisCol = disk.col + offset_col;
-
-    // capture until blank or you-colored disk
-    while ((0 <= thisRow) && (thisRow < GRID_SIZE) && (0 <= thisCol) && (thisCol < GRID_SIZE) && (diskList[thisRow][thisCol].state == otherColor)) {
-        list.push(diskList[thisRow][thisCol]);
-        thisRow += offset_row;
-        thisCol += offset_col;
-    }
-
-    // if your disk is on the other side, you can capture
-    if ((0 <= thisRow) && (thisRow < GRID_SIZE) && (0 <= thisCol) && (thisCol <= GRID_SIZE - 1) && (diskList[thisRow][thisCol].state == yourColor)) {
-        return (list)
-    } else {
-        return ([])
-    }
-
-}
-
-function playTurn(capturedList, event) {
-    let p, sum=0;
-    if (playerTurn == "yellow") {
+function playTurn(row, col, event) {
+    if (PLAYER_TURN == "yellow") {
         NUMBER_OF_YELLOW += 1;
-        p = 1;
     } else {
         NUMBER_OF_RED += 1;
-        p = -1;
-    }
-    for (let disk of capturedList) {
-        disk.state = playerTurn;
-        NUMBER_OF_YELLOW += p;
-        NUMBER_OF_RED -= p;
-        sum += 1;
     }
 
-    let s = (sum==1 ? "" : "s");
-    let ve = (sum==1 ? "s" : "ve");
-    let sentence = sum+" disk"+s+" ha"+ve+" been captured."
+    DISK_LIST[row][col].state = PLAYER_TURN;
+    DISK_PER_COLUMN[col] += 1
+
+    var player = (PLAYER_TURN == "yellow" ? "Yellow" : "Red");
+    var sentence = player + " has played in column " + (col + 1);
 
     clearHighlight();
+    overwriteGameInfo(sentence);
+    checkForWin(row, col);
     nextTurn();
     highlightGrid(event);
-    overwriteGameInfo(sentence);
-    checkForWin(playerTurn);
 }
 
-function isThereAvailableTile() {
-    for (let i = 0; i < GRID_SIZE; i++) {
-        for (let j = 0; j < GRID_SIZE; j++) {
-            if (isTilePossible(diskList[i][j]).length != 0) {
-                return (true)
-            }
+function checkInDirection(row, col, offset_row, offset_col) {
+    var playerChecked = PLAYER_TURN;
+    let r = row;
+    let c = col;
+    let count = 0;
+    while (count < 4 && (0 <= r && r < GRID_SIZE) && (0 <= c && c < GRID_SIZE)) {
+        if (DISK_LIST[r][c].state != playerChecked) {
+            return false;
         }
+
+        r += offset_row;
+        c += offset_col;
+        count++;
     }
-    return (false)
+    return (count == 4);
 }
 
-function checkForWin(playerTurn) {
-    let winCondition;
+function checkForConnectFour(row, col) {
+    var playerChecked = PLAYER_TURN;
+    var bool = false;
+    // Trigonometry-like rotation to check
+    bool = bool || checkInDirection(row, col, 1, 0);
+    bool = bool || checkInDirection(row, col, 1, 1);
+    bool = bool || checkInDirection(row, col, 0, 1);
+    bool = bool || checkInDirection(row, col, -1, 1);
+    bool = bool || checkInDirection(row, col, -1, 0);
+    bool = bool || checkInDirection(row, col, -1, -1);
+    bool = bool || checkInDirection(row, col, 0, -1);
+    bool = bool || checkInDirection(row, col, 1, -1);
+    return (bool ? playerChecked : "");
+}
+
+function checkForWin(row, col) {
+    let boardFull = false;
+    let winner = checkForConnectFour(row, col)
     if (NUMBER_OF_YELLOW + NUMBER_OF_RED == GRID_SIZE ** 2) {
-        winCondition = "boardFull";
-    } else if (!isThereAvailableTile()) {
-        winCondition = "noAvailableTile"
-    } else {
-        return
+        boardFull = true;
     }
 
-    if (NUMBER_OF_RED > NUMBER_OF_YELLOW) {
+    if (winner) {
+        drawWinText(player = winner, winCondition = "");
         stopGame();
-        drawWinText(player="red", winCondition=winCondition, playerTurn=playerTurn)
-    } else if (NUMBER_OF_YELLOW > NUMBER_OF_RED) {
+    } else if (boardFull) {
+        drawWinText(player = "tie", winCondition = "boardFull");
         stopGame();
-        drawWinText(player="yellow", winCondition=winCondition, playerTurn=playerTurn)
     } else {
-        stopGame();
-        drawWinText(player="tie", winCondition=winCondition, playerTurn=playerTurn);
+        return;
     }
-
 }
 
 function overwriteGameInfo(text) {
@@ -427,29 +416,25 @@ function overwriteGameInfo(text) {
 }
 
 function newGame() {
-    playerTurn = "red";
-    diskList = [];
-    moveList = [];
+    initGrid();
+    STARTING_PLAYER = ((!STARTING_PLAYER || STARTING_PLAYER == "red") ? "yellow" : "red");
+    PLAYER_TURN = STARTING_PLAYER;
+    DISK_LIST = [];
+    MOVE_LIST = [];
+    DISK_PER_COLUMN = Array(GRID_SIZE).fill(0);
     for (let i = 0; i < GRID_SIZE; i++) { // row
-        diskList[i] = []
+        DISK_LIST[i] = []
         for (let j = 0; j < GRID_SIZE; j++) { // column
-            diskList[i][j] = new Disk(i, j);
+            DISK_LIST[i][j] = new Disk(i, j);
         }
     }
 
-    m = Math.floor(GRID_SIZE / 2) - 1;
-
-    diskList[m][m].state = "yellow";
-    diskList[m + 1][m + 1].state = "yellow";
-    diskList[m][m + 1].state = "red";
-    diskList[m + 1][m].state = "red";
-
-    NUMBER_OF_RED = 2;
-    NUMBER_OF_YELLOW = 2;
-
+    NUMBER_OF_RED = 0;
+    NUMBER_OF_YELLOW = 0;
 }
 
 function stopGame() {
+    PLAYER_TURN = "";
     clearInterval(runningLoop);
 }
 
@@ -470,7 +455,6 @@ function startLoop() {
 
 var runningLoop;
 startLoop();
-
 
 function loop() {
     drawBoard(); // draw background
